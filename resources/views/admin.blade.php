34dc8939c7f432fc125aa0be221d93e2e64c6486
yoniffafan <yoniffafan@gmail.com>
2017-06-02 21:01:19 +0700

    wololo

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Admin page</title>
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/animate.min.css" rel="stylesheet">
+    <link href="css/font-awesome.min.css" rel="stylesheet">
+    <link href="css/lightbox.css" rel="stylesheet">
+    <link href="css/main.css" rel="stylesheet">
+    <link id="css-preset" href="css/presets/preset5.css" rel="stylesheet">
+    <link href="css/responsive.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="form-group">
+      <input type="text" name="subject" class="form-control" placeholder="Subject" required="required">
+    </div>
+    <div class="form-group">
+      <textarea name="message" id="message" class="form-control" rows="4" placeholder="Enter your message" required="required"></textarea>
+    </div>
+    <div class="form-group">
+      <input type="file" id="fileInput" name="fileInput" />
+      <button type="submit" class="btn-submit">Send Now</button>
+    </div>
+  </body>
+</html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -656,7 +656,7 @@
               <div id="post-carousel"  class="carousel slide" data-ride="carousel">
                 <ol class="carousel-indicators">
                   <li data-target="#post-carousel" data-slide-to="0" class="active"></li>
-                  <li data-target="#post-carousel" data-slide-to="1"></li>
+                  <li data-target="#post-carousel" class="" data-slide-to="1"></li>
                   <li data-target="#post-carousel" data-slide-to="2"></li>
                 </ol>
                 <div class="carousel-inner">
